877da1ae464cfa513cf8d693dd345a241d0a430e
Daljit Singh <daljitsingh101@gmail.com>
2025-02-20 20:21:59 +0530

    updated version of angular to 16 and change of base url

--- src/app/gaurds/auth.guard.ts
+++ src/app/gaurds/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from '../services/auth/auth.service';
 import { Observable } from 'rxjs';
 import { map, take } from 'rxjs/operators';
@@ -7,7 +7,7 @@ import { map, take } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(): Observable<boolean> {
