4ba45913de58bf53df486a79a1c026801bbdd10d
Daljit Singh <daljitsingh101@gmail.com>
2025-02-20 20:28:01 +0530

    updated version of angular to 16 and change of base url

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -9,7 +9,7 @@ import { map, tap } from 'rxjs/operators';
 })
 export class AuthService {
   private isAuthenticated = new BehaviorSubject<boolean>(this.hasToken());
-  private apiUrl = ' https://project.surveywerkx.com/admin/login';
+  private apiUrl = 'https://project.surveywerkx.com/admin/login';
 
   constructor(private http: HttpClient, private router: Router) { }
 
